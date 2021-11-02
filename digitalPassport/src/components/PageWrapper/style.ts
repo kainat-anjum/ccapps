2b787fe7568841be9e7c957cc3164ff3de508153
Muhammad Talha <muhammadtalha04@gmail.com>
2021-11-02 11:03:04 +0500

    Added slim scroll

--- digitalPassport/src/components/PageWrapper/style.ts
+++ digitalPassport/src/components/PageWrapper/style.ts
@@ -1,6 +1,27 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 type CommonProps = { isSplashScreen: boolean }
+
+const SlimScrollStyle = css`
+    &::-webkit-scrollbar {
+        width: 0.6rem;
+    }
+
+    &::-webkit-scrollbar-track {
+        -webkit-box-shadow: none;
+        box-shadow: none;
+        border-radius: 1rem;
+        border-right: 0.5rem solid transparent;
+        background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        -webkit-border-radius: 1rem;
+        border-radius: 1rem;
+        height: 2rem;
+        background: #a5b0bf;
+    }
+`
 
 export const PageWrapperStyled = styled.section<CommonProps>`
     position: relative;
@@ -23,4 +44,6 @@ export const ScrollableContentWrapper = styled.section<CommonProps>`
     overflow-y: auto;
     overflow-x: hidden;
     padding-bottom: 10px;
+
+    ${SlimScrollStyle}
 `
